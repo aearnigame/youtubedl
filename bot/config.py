b4bd28935338f4aef58563fe4c43cb383d7f31b9
aearnigame <106689620+aearnigame@users.noreply.github.com>
2022-06-11 11:23:15 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,46 +3,46 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("5303569366:AAGJp5az30bftwqR2c_59tDHCyez_qb-iGE")
 
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
+    SESSION_NAME = os.environ.get("aebot", ":memory:")
 
-    API_ID = int(os.environ.get("API_ID"))
+    API_ID = int(os.environ.get("13689511"))
 
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("c4527c8e214ef2450e352ce3a24a7454")
 
-    CLIENT_ID = os.environ.get("CLIENT_ID")
+    CLIENT_ID = os.environ.get("339369275959-k3jgbbql83fcaci9sq7srf69cgjnhsjp.apps.googleusercontent.com")
 
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+    CLIENT_SECRET = os.environ.get("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")
 
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    BOT_OWNER = int(os.environ.get("1301592091"))
 
-    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
+    AUTH_USERS_TEXT = os.environ.get("1301592091", "5368797628","1942827785")
 
-    AUTH_USERS = [BOT_OWNER, 374321319] + (
+    AUTH_USERS = [BOT_OWNER, 1301592091] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
         if AUTH_USERS_TEXT
         else []
     )
 
     VIDEO_DESCRIPTION = (
-        os.environ.get("VIDEO_DESCRIPTION", "").replace("<", "").replace(">", "")
+        os.environ.get("VIDEO_DESCRIPTION", "anirudhyeji").replace("<", "").replace(">", "")
     )
 
     VIDEO_CATEGORY = (
         int(os.environ.get("VIDEO_CATEGORY")) if os.environ.get("VIDEO_CATEGORY") else 0
     )
 
-    VIDEO_TITLE_PREFIX = os.environ.get("VIDEO_TITLE_PREFIX", "")
+    VIDEO_TITLE_PREFIX = os.environ.get("VIDEO_TITLE_PREFIX", "anirudh_karamungikar")
 
-    VIDEO_TITLE_SUFFIX = os.environ.get("VIDEO_TITLE_SUFFIX", "")
+    VIDEO_TITLE_SUFFIX = os.environ.get("VIDEO_TITLE_SUFFIX", "anirudhyeji")
 
     DEBUG = bool(os.environ.get("DEBUG"))
 
     UPLOAD_MODE = os.environ.get("UPLOAD_MODE") or False
     if UPLOAD_MODE:
         if UPLOAD_MODE.lower() in ["private", "public", "unlisted"]:
-            UPLOAD_MODE = UPLOAD_MODE.lower()
+            UPLOAD_MODE = UPLOAD_MODE.lower("private","public")
         else:
             UPLOAD_MODE = False
 
